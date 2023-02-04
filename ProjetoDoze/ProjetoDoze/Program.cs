81289584a467d56242782ebaa3f497516e0f833d
Victor Dubeux <adv.victordubeux@gmail.com>
2023-02-04 13:08:02 -0300

    P12 C2 - Envio definitivo de projeto

---
+++ ProjetoDoze/ProjetoDoze/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Exercicio
+{
+    class ProjetoDoze
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite um valor de linhas. Cada linha conterá o respectivo valor elevado ao quadrado e ao cubo.");
+            int numLinhas = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= numLinhas; i++)
+            {
+                Console.Write("{0}: ", i);
+                Console.Write($"{Math.Pow(i, 2)} ");
+                Console.WriteLine(Math.Pow(i, 3));
+            }
+            Console.WriteLine("Fim!");
+
+            Console.ReadKey();
+        }
+    }
+}
